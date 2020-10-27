481738bf25e83566416cb30a20f0a818186fbbea
Barry <bboretzky@gmail.com>
2020-10-27 13:05:00 -0700

    Initial Commit

---
+++ API/SQL Queries/SET IDENTITY_INSERT [SkiNet].[dbo].[Prod.sql
@@ -0,0 +1,8 @@
+SET IDENTITY_INSERT [SkiNet].[dbo].[Products] ON;
+
+INSERT INTO [SkiNet].[dbo].[Products]
+    (Id,Name)
+VALUES
+    (1, 'Product one'),
+    (2, 'Product two'),
+    (3, 'Product three');

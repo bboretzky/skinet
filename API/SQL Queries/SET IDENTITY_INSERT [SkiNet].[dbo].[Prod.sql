8acadbdfe079194ca46939c51b058a40ec4e08a8
Barry <bboretzky@gmail.com>
2020-10-28 15:08:23 -0700

    End of Section 3

--- API/SQL Queries/SET IDENTITY_INSERT [SkiNet].[dbo].[Prod.sql
+++ API/SQL Queries/SET IDENTITY_INSERT [SkiNet].[dbo].[Prod.sql
@@ -1,8 +1,10 @@
-SET IDENTITY_INSERT [SkiNet].[dbo].[Products] ON;
+-- SET IDENTITY_INSERT [SkiNet].[dbo].[Products] ON;
+SET IDENTITY_INSERT [SkiNet].[dbo].[ProductBrands] ON
+-- SET IDENTITY_INSERT [SkiNet].[dbo].[ProductTypes] ON;
 
-INSERT INTO [SkiNet].[dbo].[Products]
-    (Id,Name)
-VALUES
-    (1, 'Product one'),
-    (2, 'Product two'),
-    (3, 'Product three');
+-- INSERT INTO [SkiNet].[dbo].[Products]
+--     (Id,Name)
+-- VALUES
+--     (1, 'Product one'),
+--     (2, 'Product two'),
+--     (3, 'Product three');
